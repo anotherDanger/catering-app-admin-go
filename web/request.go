9cac31c4d3fe3ef7e4560299328cb7df851096bd
Andhika Danger <andhikadanger666@gmail.com>
2025-06-03 13:02:52 +0700

    web utils

---
+++ web/request.go
@@ -0,0 +1,12 @@
+package web
+
+import "time"
+
+type Request struct {
+	Id          string    `json:"id" validate:"required,uuid"`
+	Name        string    `json:"name" validate:"alpha,required,string,min=5,max=50"`
+	Description string    `json:"description" validate:"alphanum"`
+	Stock       int       `json:"stock" validate:"required,number"`
+	Price       int       `json:"price" validate:"required,number"`
+	CreatedAt   time.Time `json:"created_at" validate:"required"`
+}
