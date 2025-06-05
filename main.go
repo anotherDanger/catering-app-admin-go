8fb1520d32bef0e7fa925e7569fa67a06899e122
Andhika Danger <andhikadanger666@gmail.com>
2025-06-06 05:30:57 +0700

    dependency injection

--- main.go
+++ main.go
@@ -2,30 +2,30 @@ package main
 
 import (
 	"catering-admin-go/controller"
-	"catering-admin-go/helper"
-	"catering-admin-go/repository"
-	"catering-admin-go/service"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
-func main() {
+func NewServer(handler controller.Controller) *fiber.App {
 	app := fiber.New()
 	app.Use(cors.New())
 
-	db, cleanup, _ := helper.NewDb()
-	repo := repository.NewRepositoryImpl()
-	svc := service.NewServiceImpl(repo, db)
-	ctrl := controller.NewControllerImpl(svc)
+	app.Post("/v1/products", handler.AddProduct)
+	app.Get("/v1/products", handler.GetProducts)
+	app.Delete("/v1/products/:id", handler.DeleteProduct)
+	app.Put("/v1/products/:id", handler.UpdateProduct)
+
+	return app
+}
+
+func main() {
+	app, cleanup, err := InitServer()
+	if err != nil {
+		panic(err)
+	}
 
 	defer cleanup()
 
-	app.Post("/v1/products", ctrl.AddProduct)
-	app.Get("/v1/products", ctrl.GetProducts)
-	app.Delete("/v1/products/:id", ctrl.DeleteProduct)
-	app.Put("/v1/products/:id", ctrl.UpdateProduct)
-
 	app.Listen(":8080")
-
 }
